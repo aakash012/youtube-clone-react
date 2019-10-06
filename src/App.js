36eed2cd3f32345d0659f202a14e66cceb904147
Aakash kumar <aakash012@live.com>
2019-10-06 19:28:11 +0530

    Video Container Component added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, {Component} from 'react';
 import {Home} from './containers/Home/Home';
 import {AppLayout} from './components/AppLayout/AppLayout';
 import {Route, Switch} from 'react-router-dom';
+import {Watch} from '../src/containers/Watch/Watch';
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
-       <Route path="/watch" component={Watch}/>
-       <Route path="/" component={Home}/>
+        <Switch>
+        <Route path="/watch" component={Watch}/>
+        <Route path="/" component={Home}/>
+        </Switch>
       </AppLayout>
     );
   }
