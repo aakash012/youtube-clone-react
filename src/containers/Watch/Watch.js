49cf3d35c88b064efa41fe9a8290338c2206c506
Aakash kumar <aakash012@live.com>
2019-10-09 23:27:44 +0530

    Added Comments component styled it

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -4,17 +4,17 @@ import {RelatedVideos} from '../../components/RelatedVideos/RelatedVideos';
 import {Video} from '../../components/Video/Video';
 import {VideoMetadata} from '../../components/VideoMetadata/VideoMetadata';
 import {VideoInfoBox} from '../../components/VideoInfoBox/VideoInfoBox';
+import {Comments} from '../Comments/Comments';
 
 export class Watch extends React.Component {
-  // stretch?
   render() {
     return (
       <div className='watch-grid'>
         <Video className='video' id='-7fuHEEmEjs'/>
         <VideoMetadata className='metadata' viewCount={1000}/>
         <VideoInfoBox className='video-info-box'/>
-        <div className='comments' style={{width: '100%', height: '100px', background: '#9013FE'}}>comments</div>
-        <RelatedVideos className='related-videos'/>
+        <Comments className='comments'/>
+        <RelatedVideos className='relatedVideos'/>
       </div>
     );
   }
